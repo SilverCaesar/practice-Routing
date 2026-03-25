a19c2e9d4a978e4f7b52668ee2e348b7e1675698
Julius Rosalin <juliusrosalin@gmail.com>
2026-03-25 11:51:28 -0500

    3/25/26 class activity

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { DataService } from '../data.service';
 
 
 @Component({
   selector: 'app-post-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
 export class PostListComponent {
-
+  dataService = inject(DataService);
 }
